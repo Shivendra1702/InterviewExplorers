9c07bac92b95b191b42ef445351048712b5e0ad7
Shivendra1702 <shivendrapratapjadia@gmail.com>
2023-10-01 17:57:42 +0530

    added build

--- api/models/User.js
+++ api/models/User.js
@@ -47,7 +47,9 @@ userSchema.methods.isValidPassword = async function (userSendPassword) {
 };
 
 userSchema.methods.getJwtToken = function () {
-  return jwt.sign({ id: this._id }, process.env.JWT_SECRET);
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRY,
+  });
 };
 
 const User = mongoose.model("User", userSchema);
